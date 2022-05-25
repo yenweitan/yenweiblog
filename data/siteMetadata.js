4037dba74cae3c968e6bb325fc998c654051e2b5
Yen Wei <yenwei9761@gmail.com>
2022-05-25 21:09:37 +0800

    metadata tweaks for giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'My little corner in the vast parts of the internet',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://yenweitan.com,',
-  siteRepo: 'https://github.com/yenweitan/yenweiblog2.0',
+  siteUrl: 'https://yenweiblog.vercel.app/,',
+  siteRepo: 'https://github.com/yenweitan/yenweiblog',
   siteLogo: '',
   image: '/static/images/yenwei.jpeg',
   socialBanner: '',
@@ -35,10 +35,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'yenweitan/yenweiblog',
+      repositoryId: 'R_kgDOHZNwvA',
+      category: 'Comments',
+      categoryId: 'DIC_kwDOHZNwvM4CPSPH',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
@@ -47,7 +47,7 @@ const siteMetadata = {
       // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: 'dark_protanopia',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
